fde9cd7e5d86a030d35962637b534bf0ce439a4e
KwonJaehoon <wogns0197@gmail.com>
2023-04-22 20:38:28 +0900

    init4

---
+++ Source/ProjectD/ProjectD.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProjectD : ModuleRules
+{
+	public ProjectD(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/ProjectD/ProjectDGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectDGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectDGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectDGameMode();
+};
+
+
+
